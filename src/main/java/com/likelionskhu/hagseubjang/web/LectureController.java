58601954e4a26ae7a0d6a886a25bc9d4735a6af3
djdongjae <isf1999@naver.com>
2023-08-03 18:00:11 +0900

    강좌 api로 업데이트 로직 개발

--- src/main/java/com/likelionskhu/hagseubjang/web/LectureController.java
+++ src/main/java/com/likelionskhu/hagseubjang/web/LectureController.java
@@ -9,8 +9,11 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -20,6 +23,7 @@ public class LectureController {
     @Autowired
     private LectureRepository lectureRepository;
 
+    // 파일 데이터로 강좌 초기화
     @RequestMapping("load_save")
     public String load_save() throws Exception {
         JSONParser parser = new JSONParser();
@@ -45,7 +49,10 @@ public class LectureController {
             String referenceDate1 = ((String) tmp.get("데이터기준일자")).length() == 0 ? "1000-01-01" : (String) tmp.get("데이터기준일자");
             LocalDate referenceDate2 = LocalDate.parse(referenceDate1, DateTimeFormatter.ISO_DATE);
 
-            if (edcEndDay2.isAfter(LocalDate.of(2023, 1, 1)) && referenceDate2.isAfter(LocalDate.of(2022, 6, 6))) {
+            if (
+                    edcEndDay2.isAfter(LocalDate.of(2023, 1, 1))
+                            && referenceDate2.isAfter(LocalDate.of(2022, 6, 6))
+            ) {
                 Lecture lectureInfo = new Lecture(
                         i + 1,
                         (String) tmp.get("강좌명"),
@@ -81,5 +88,40 @@ public class LectureController {
         }
         return "success";
 
+    }
+
+    // api로 강좌 업데이트
+    public void lecture_update() throws Exception {
+        String result;
+
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String referenceDate = today.format(dateTimeFormatter);
+
+        URL url = new URL(
+                "http://api.data.go.kr/openapi/tn_pubr_public_lftm_lrn_lctre_api"
+                + "?serviceKey=U%2BFAmDElGz%2BqU3DnkL9KnewJSgBy7chmjHrqGFnZJIZF2DjSKEB0jHObepr4LW%2B5HVRVN%2BAPVVNdFfSle2m%2FBw%3D%3D"
+                + "&pageNo=1&numOfRows=100"
+                + "&type=json"
+                + "referenceDate=" + referenceDate
+        );
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+        result = bf.readLine();
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
+        JSONObject response = (JSONObject) jsonObject.get("response");
+        JSONObject header = (JSONObject) response.get("header");
+        String resultCode = (String) header.get("resultCode");
+        if (resultCode.equals("00")) {
+            JSONObject body = (JSONObject) response.get("body");
+            JSONArray items = (JSONArray) body.get("items");
+
+            for (int i = 0; i < items.size(); i++) {
+                JSONObject tmp = (JSONObject) items.get(i);
+                
+            }
+        }
     }
 }
