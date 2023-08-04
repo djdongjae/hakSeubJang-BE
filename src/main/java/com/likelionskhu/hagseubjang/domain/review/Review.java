9b11b1b311de80b30e2849511d7f3340d992631e
djdongjae <isf1999@naver.com>
2023-08-04 17:12:43 +0900

    후기 작성 기능 구현

---
+++ src/main/java/com/likelionskhu/hagseubjang/domain/review/Review.java
@@ -0,0 +1,47 @@
+package com.likelionskhu.hagseubjang.domain.review;
+
+import com.likelionskhu.hagseubjang.domain.BaseTimeEntity;
+import com.likelionskhu.hagseubjang.domain.lecture.Lecture;
+import com.likelionskhu.hagseubjang.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Review extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "lectureId")
+    Lecture lecture;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    User user;
+
+    @Builder
+    public Review(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
