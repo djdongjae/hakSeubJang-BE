993182c2239e220ab4a609f035538d8daa3e0e4c
djdongjae <jk47wee@gmail.com>
2023-09-10 23:04:55 +0900

    modify loginSuccessURL

--- src/main/java/com/likelionskhu/hagseubjang/config/auth/SecurityConfig.java
+++ src/main/java/com/likelionskhu/hagseubjang/config/auth/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .formLogin()
                     .loginPage("/user/login")
-                    .defaultSuccessUrl("/")
+//                    .defaultSuccessUrl("/")
                     .permitAll()
                 .and()
                     .logout()
